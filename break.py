26bdd141a8627c868fa49b21709678a792d6876d
YashPrateek27 <149778133+YashPrateek27@users.noreply.github.com>
2023-12-01 10:32:36 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,25 @@
+'''for i in range (1,11):
+     if i==5:
+                   break
+     print(i)'''
+
+
+
+
+
+'''T2=('cherey','Banana','Apple','Orange')
+for i in T2:
+     if 'pp' in i:
+         print(f"found 'pp' in (i)")
+         break
+     print(i)
+else:
+        print("Further identitation cannot be run")'''
+
+
+
+for i in a
+
+        
+         
+    
